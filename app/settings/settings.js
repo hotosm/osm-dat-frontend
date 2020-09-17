1245760b5d61d68520a468f88131aeab5e93d1f3
Martin Raifer <martin.raifer@heigit.org>
2020-09-17 09:31:07 +0200

    switch to new mapbox-light tiles, fixes #107

--- app/settings/settings.js
+++ app/settings/settings.js
@@ -4,7 +4,7 @@ export default {
   'vt-source': 'https://tiles.osm-analytics.heigit.org', // source of current vector tiles
   'vt-gaps-source': 'https://tiles.osm-analytics.heigit.org/gaps',
   'vt-hist-source': 'https://tiles.osm-analytics.heigit.org', // source of historic vector tiles for compare feature
-  'map-background-tile-layer': 'https://api.mapbox.com/v4/mapbox.light/{z}/{x}/{y}.png?access_token=' + mapbox_token,
+  'map-background-tile-layer': 'https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token=' + mapbox_token,
   'map-attribution': '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>',
   'tm-api': 'https://tasking-manager-tm4-production-api.hotosm.org/api/v2', // hot tasking manager api
 }
